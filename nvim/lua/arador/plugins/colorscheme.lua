ca9ae827395cdaf43c0eddaf3e3dcaeeec6f775e
arador1 <chaitanyachandu523@gmail.com>
2025-02-17 12:00:28 +0530

    i3 update

---
+++ nvim/lua/arador/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		flavour = "frappe",
+		config = function()
+			vim.cmd("colorscheme catppuccin-frappe")
+		end,
+	},
+}
---
+++ nvim/lua/arador/plugins/formatter.lua
@@ -0,0 +1,22 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
+
+		conform.setup({
+			formatters_by_ft = {
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				lua = { "stylua" },
+				python = { "isort", "black" },
+				java = { "google-java-format" },
+				c = { "clang-format" },
+				cpp = { "clang-format" },
+				bash = { "shfmt" },
+				rust = { "rustfmt" },
+			},
+			format_on_save = true,
+		})
+	end,
+}
