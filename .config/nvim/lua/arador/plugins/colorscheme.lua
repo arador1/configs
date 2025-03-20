fe9970309e136dbe21782f5f1e1f05661c27b711
arador1 <chaitanyachandu523@gmail.com>
2025-03-20 14:05:12 +0530

    upp

---
+++ .config/nvim/lua/arador/plugins/colorscheme.lua
@@ -0,0 +1,39 @@
+-- Sonokai Theme
+return {
+	{ --   -- https://github.com/sainnhe/sonokai
+		"sainnhe/sonokai",
+		lazy = false, -- We want the colorscheme to load immediately when starting Neovim
+		priority = 1000, -- Load the colorscheme before other non-lazy-loaded plugins
+		config = function(_, opts)
+			vim.g.sonokai_style = "default" -- "default, atlantis, andromeda, shusia, maia, espresso"
+			--	vim.cmd("colorscheme sonokai") -- Replace this with your favorite colorscheme
+		end,
+	},
+
+	{
+		"shaunsingh/nord.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			-- Enable Nord colorscheme
+			--		vim.cmd("colorscheme nord")
+
+			-- Set Nord-specific options
+			vim.g.nord_contrast = true
+			vim.g.nord_borders = false
+			vim.g.nord_disable_background = false
+			vim.g.nord_italic = true
+			vim.g.nord_uniform_diff_background = true
+			vim.g.nord_bold = true
+		end,
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		flavour = "frappe",
+		config = function()
+			vim.cmd("colorscheme catppuccin-frappe")
+		end,
+	},
+}
--- .zshrc
+++ .zshrc
@@ -1,1 +1,21 @@
-/home/arador/.zshrc
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+# Created by newuser for 5.9
+source ~/powerlevel10k/powerlevel10k.zsh-theme
+source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+#####
+export EDITOR='nvim'
+export IMAGE_VIEWER='imv'
+
+#alias
+alias ls='exa --icons --color=always --group-directories-first'
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
